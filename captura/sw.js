d8b53fd246836eaeb99871612ccb9abe8fa58109
mascochicle <mascochicle@users.noreply.github.com>
2026-08-20 11:12:03 -0600

    App de captura: boton Mandar a Drive, comparte un .txt con fecha en el nombre

--- captura/sw.js
+++ captura/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "captura-v19";
+const CACHE = "captura-v94";
 const ARCHIVOS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
