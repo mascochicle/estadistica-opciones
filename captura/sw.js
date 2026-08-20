827fbbfcd18dbfa239faa7fef807cb4cd79a45f1
mascochicle <mascochicle@users.noreply.github.com>
2026-08-20 11:21:12 -0600

    App de captura: envio automatico a Drive por un Apps Script propio

--- captura/sw.js
+++ captura/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "captura-v94";
+const CACHE = "captura-v95";
 const ARCHIVOS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
